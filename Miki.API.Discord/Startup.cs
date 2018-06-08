49156ec347cf2ca57c8f2f4d1855adf676fb6245
Veld <velddev@gmail.com>
2018-06-09 01:52:54 +0200

    Add: json formatters

--- Miki.API.Discord/Startup.cs
+++ Miki.API.Discord/Startup.cs
@@ -27,7 +27,9 @@ namespace Miki.WebAPI.Discord
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-			services.AddMvcCore();
+			services
+				.AddMvcCore()
+				.AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
